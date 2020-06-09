eaebaed5e9b53c037024b4153ade7a5b2cd9fd95
KonovalovAlex <alexnicetomeetu@gmail.com>
2020-06-09 12:18:10 +0600

    commit

---
+++ src/main/java/tutorials/exceptions/Main.java
@@ -0,0 +1,11 @@
+package tutorials.exceptions;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        f();
+    }
+    public static void f() throws RuntimeException {
+    }
+}
